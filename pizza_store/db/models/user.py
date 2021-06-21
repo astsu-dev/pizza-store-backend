09678cf5f922cc2f749159f911b9cf397374de44
astsu <you@example.com>
2021-06-21 19:16:48 +0300

    Make User db model fields is not nullable.

--- pizza_store/db/models/user.py
+++ pizza_store/db/models/user.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username = Column(String(30), unique=True)
-    email = Column(String(50), unique=True)
-    password_hash = Column(String)
-    role = Column(String(30))
+    username = Column(String(30), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(String(30), nullable=False)
