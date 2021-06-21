050f1efd0782f059578c03cbb77a19007a5d7222
astsu <you@example.com>
2021-06-21 17:24:33 +0300

    Fix syntax error.

--- pizza_store/db/models/user.py
+++ pizza_store/db/models/user.py
@@ -8,7 +8,7 @@ from sqlalchemy.dialects.postgresql import UUID
 class User(Base):
     __tablename__ = "users"
 
-    id: Column[UUID] = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String(30), unique=True)
     email = Column(String(50), unique=True)
     password_hash = Column(String)
