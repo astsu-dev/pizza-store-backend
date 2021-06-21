d15f3625d461cd7e94077da3822fd12878c3995a
astsu <you@example.com>
2021-06-21 16:25:25 +0300

    Remove Permission, Role, RolePermission db models. Add User db model.

--- pizza_store/db/models/__init__.py
+++ pizza_store/db/models/__init__.py
@@ -1,4 +1,4 @@
 from pizza_store.db.models.category import Category
-from pizza_store.db.models.user import Permission, Role, RolePermission
+from pizza_store.db.models.user import User
 
-__all__ = ["Category", "Permission", "Role", "RolePermission"]
+__all__ = ["Category", "User"]
--- pizza_store/db/models/user.py
+++ pizza_store/db/models/user.py
@@ -1,24 +1,15 @@
+import uuid
+
 from pizza_store.db.db import Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
 
 
-class Permission(Base):
-    __tablename__ = "permissions"
+class User(Base):
+    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30), unique=True, nullable=False)
-
-
-class Role(Base):
-    __tablename__ = "roles"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30), unique=True, nullable=False)
-
-
-class RolePermission(Base):
-    __tablename__ = "roles_permissions"
-
-    role_id = Column(Integer, ForeignKey("roles.id"), primary_key=True)
-    permission_id = Column(Integer, ForeignKey("permissions.id"), primary_key=True)
+    id: Column[UUID] = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String(30), unique=True)
+    email = Column(String(50), unique=True)
+    password_hash = Column(String)
+    role = Column(String(30))
