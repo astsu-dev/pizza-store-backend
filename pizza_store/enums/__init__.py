11f8a471d39b04de647a201f2c18c54c85e50e8b
astsu <you@example.com>
2021-07-10 08:40:08 +0300

    Refactor: add __all__ to enums package.

--- pizza_store/enums/__init__.py
+++ pizza_store/enums/__init__.py
@@ -1,2 +1,4 @@
 from pizza_store.enums.permissions import CategoryPermission, ProductPermission
 from pizza_store.enums.role import Role
+
+__all__ = ["CategoryPermission", "ProductPermission", "Role"]
