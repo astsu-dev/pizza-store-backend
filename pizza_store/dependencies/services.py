7bece57a4d7a98334e7c6cbf47171cb9fe450fb5
astsu <you@example.com>
2021-08-06 12:21:55 +0300

    Add get_category_service dependency.

--- pizza_store/dependencies/services.py
+++ pizza_store/dependencies/services.py
@@ -1,7 +1,12 @@
 from fastapi import Depends
-from pizza_store.db.crud import RefreshTokenCRUD, UserCRUD
+from pizza_store.db.crud import CategoryCRUD, RefreshTokenCRUD, UserCRUD
 from pizza_store.dependencies.db import get_session
-from pizza_store.services import AuthService, IAuthService
+from pizza_store.services import (
+    AuthService,
+    CategoryService,
+    IAuthService,
+    ICategoryService,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -18,3 +23,19 @@ def get_auth_service(session: AsyncSession = Depends(get_session)) -> IAuthServi
     user_crud = UserCRUD()
     refresh_token_crud = RefreshTokenCRUD()
     return AuthService(session, user_crud, refresh_token_crud)
+
+
+def get_category_service(
+    session: AsyncSession = Depends(get_session),
+) -> ICategoryService:
+    """Returns instance of category service.
+
+    Args:
+        session (AsyncSession, optional): sqlalchemy session
+
+    Returns:
+        ICategoryService
+    """
+
+    category_crud = CategoryCRUD()
+    return CategoryService(session, category_crud)
