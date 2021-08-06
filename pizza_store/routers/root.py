69cd5994ca965062fc6ae58e85a1d05bca52ddce
astsu <you@example.com>
2021-08-06 12:22:38 +0300

    Add "/api" prefix to root router. Include category router to root router.

--- pizza_store/routers/root.py
+++ pizza_store/routers/root.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from pizza_store.routers.auth import auth_router
+from pizza_store.routers.category import category_router
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 router.include_router(auth_router)
+router.include_router(category_router)
