9818caed5e3e996460ad3c898e09d3f3542927be
astsu <you@example.com>
2021-08-08 12:05:33 +0300

    Add IMAGE_READ_BUFFER, STATIC_FOLDER_PATH, IMAGE_FOLDER_PATH constants.

---
+++ pizza_store/constants/files.py
@@ -0,0 +1,4 @@
+from typing import Final
+
+# Buffer for reading image from client
+IMAGE_READ_BUFFER: Final[int] = 256 * 256
---
+++ pizza_store/constants/paths.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+from typing import Final
+
+STATIC_FOLDER_PATH: Final[Path] = Path("static")
+IMAGE_FOLDER_PATH: Final[Path] = STATIC_FOLDER_PATH / "img"
